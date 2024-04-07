1738443f7da1f6daf7be8702452cd958fe724ed0
Ranesha Pal <raneshapal@gmail.com>
2024-04-07 23:44:31 +0530

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Sentry from '@sentry/browser'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'privateCircle';
+
+  constructor(){
+    // top row
+Sentry.init({
+  dsn: "https://9c290a5f5c76ce7da39b20da3956ab59@o4507045655805952.ingest.us.sentry.io/4507045670223872",
+});
+
+console.log('initiated')
+setTimeout(()=>{
+  throw new Error("Sentry test")
+},5000)
+  }
 }
